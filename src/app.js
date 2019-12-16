feefbe644bd6ccfd58aefae98c6e9981ade362b7
MattWheeler10 <matthewwheeler66@gmail.com>
2019-12-16 10:18:27 +0000

    Fixed error

--- src/app.js
+++ src/app.js
@@ -6,10 +6,6 @@ const mysql = require('mysql');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded());
-app.use(express.json());
-app.use(express.static('.'));
-
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
