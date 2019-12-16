2ff19598ccfa74acca288a3b6c2a5fe15f4f705a
Catherine Harrison <mrs.wizrd@gmail.com>
2019-12-16 15:12:16 +0000

    Made user forms

--- src/app.js
+++ src/app.js
@@ -12,6 +12,21 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
+app.get('/login', (req, res) => {
+    res.render('login');
+    console.log('Warwick Davis has allowed you access, huzzah!');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+    console.log('Warwick Davis has allowed you to register, play it safe!');
+})
+
+app.get('/updatepw', (req, res) => {
+    res.render('updatepw');
+    console.log('Warwick Davis has decided you can change your password on this occasion...');
+})
+
 app.listen(port, () => {
     console.log("[SERVER] Server is running on port " + port);
 })
