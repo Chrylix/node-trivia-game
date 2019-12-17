85c1177eb7a5c71fef46b0fe1abc31e3833be3c5
Chrylix <hall.dhall.dylan@gmail.com>
2019-12-17 12:18:34 +0000

    Fixed hanging on login

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 const request = require('request'); 
 
-const triviaFunc = (numberOfQuestions, category, difficulty, callback)=>{
+const triviaFunc = async (numberOfQuestions, category, difficulty, callback)=>{
     
     let apiUrl= `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`
 
-    request ({ url: apiUrl, json:true}, (error,response) =>{
+    await request ({ url: apiUrl, json:true}, (error,response) =>{
         if (error){
             console.log('there is an error in the api')
         } else if (response.body === undefined){
