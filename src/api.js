724b111887d13ea0c768e1a1c56d1b7acc5d7820
Chrylix <hall.dhall.dylan@gmail.com>
2019-12-18 09:58:48 +0000

    Fixed hanging on login with fallback redundancy to hide question pane on API failure

--- src/api.js
+++ src/api.js
@@ -3,16 +3,19 @@ const request = require('request');
 const triviaFunc = async (numberOfQuestions, category, difficulty, callback)=>{
     
     let apiUrl= `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`
-
-    await request ({ url: apiUrl, json:true}, (error,response) =>{
-        if (error){
-            console.log('there is an error in the api')
-        } else if (response.body === undefined){
-            console.log(`Difficulty of category don't exist`)
-        } else{
-            callback(response.body)
-        }
-    })
+    try {
+        await request ({ url: apiUrl, json:true}, (error,response) =>{
+            if (error){
+                console.log('there is an error in the api')
+            } else if (response.body === undefined){
+                console.log(`Difficulty of category don't exist`)
+            } else{
+                callback(response.body)
+            }
+        })
+    } catch(error) {
+        console.error(error);
+    }
 };
 
 module.exports = triviaFunc;
