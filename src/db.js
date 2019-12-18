70ae870d818f64621e4d0fc1edacc7c8d0dbd848
Chrylix <hall.dhall.dylan@gmail.com>
2019-12-18 16:47:14 +0000

    Fixed typo

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USERNAME || 'root',
-    password: process.env.PASSWORD ||'password',
+    password: process.env.MYSQL_PASSWORD ||'password',
     port: 3306,
     database: process.env.MYSQL_DBNAME || 'trivia',
 })
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
 db.connect( (error) => {
     if (error) {
         console.log("[ERROR] Failed to connect to the MySQL server!");
+        console.log(error);
     } else {
         console.log("[SERVER] MySQL successfully connected.")
     }
