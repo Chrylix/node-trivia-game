c9e1b5c0db55499f88f9bf49bb28cf52abd12336
Chrylix <hall.dhall.dylan@gmail.com>
2019-12-19 13:11:35 +0000

    Moved MYSQL_PORT parameter to support .env

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USERNAME || 'root',
     password: process.env.MYSQL_PASSWORD ||'password',
-    port: 3306,
+    port: process.env.MYSQL_PORT || 3306,
     database: process.env.MYSQL_DBNAME || 'trivia',
 })
 
